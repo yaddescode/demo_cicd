123793bfc4a962b2614cd05781cdd7e04124e8e4
yasha <yasha.addes@startengine.com>
2023-06-13 15:48:44 -0700

    added basic rest endpoint

---
+++ src/test/java/net/yasha/cicddemo/CicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yasha.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
